2bcb0d52c9ea1f6904281e1dbc93e478718507a1
antonako1-tutors <anton.akonpelto@gmail.com>
2024-12-03 08:53:29 +0200

    1.2 valmis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import styles from './styles/App.module.css'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div>
+    const [text, setText] = useState("")
+    return (
+        <div>
+            <input 
+                type="text" 
+                placeholder='Kirjoita jotain'
+                onChange={(e) => setText(e.target.value)}    
+            />
+            <button
+                onClick={() => navigator.clipboard.writeText(text)}
+            >Kopioi leikepöydälle</button>
 
-    </div>
-  )
+            <input type='text' placeholder='Pasteta'/>
+        </div>
+    )
 }
 
 export default App
